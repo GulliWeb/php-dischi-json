bb8ee76589205c98352b2731be6143e24f37333b
Andrea Gulli <andreagulli11@gmail.com>
2024-10-08 18:01:03 +0200

    add BONUS (overlay, overlay style, overlay animation, & overlay methods)

--- js/script.js
+++ js/script.js
@@ -1,27 +1,38 @@
 const { createApp } = Vue
 
-// Creo la mia applicazione integrata con vue
 createApp({
-    data(){
-        return{
+    data() {
+        return {
             dischi: [],
-
+            currentDisk: null,
         }
     },
-    methods:{
+    methods: {
         // Metodo per effettuare una chiamata AXIOS e stampare i dischi in pagina
-        getDisc(){
-            axios.get('http://localhost:8888/php-dischi-json/dischi.json') // Indirizzo server locale del mio file.json
-            .then((response) => {
-              this.dischi = response.data // Assegno il campo.data della risposta al mio array dischi
-              console.log(this.dischi);
-            })
-            .catch(function (error) {
-              console.log(error);
-            })
+        getDisc() {
+            axios.get('http://localhost:8888/php-dischi-json/dischi.json')
+                .then((response) => {
+                    this.dischi = response.data
+                    console.log(this.dischi);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+        },
+        getCurrentInfo(index) {
+            if (this.currentDisk && this.currentDisk.title === this.dischi[index].title) {
+                // Se la card cliccata è già aperta nascondo l'overlay
+                this.hideOverlay();
+            } else {
+                // Se non è cliccata mostro l'overlay per il disco selezionato
+                this.currentDisk = this.dischi[index];
+            }
+        },
+        hideOverlay() {
+            this.currentDisk = null; 
         }
     },
-    created(){
-        this.getDisc() // Alla creazione della pagina chiamo immediatamente la mia funzione che effettua la chiamata API e popola l'array.
+    created() {
+        this.getDisc(); // Alla creazione della pagina chiamo immediatamente la mia funzione che effettua la chiamata API e popola l'array.
     }
 }).mount('#app')
